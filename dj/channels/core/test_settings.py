dbd014a46c99212fe1dfafe57c0e2252f13259eb
Ryan Northey <ryan@synca.io>
2020-03-04 09:33:01 +0000

    Add python setup.py

---
+++ dj/channels/core/test_settings.py
@@ -0,0 +1,12 @@
+
+SECRET_KEY = "NOTSOSECRET"
+
+INSTALLED_APPS = [
+    'channels',
+    'django.contrib.admin',
+    'django.contrib.auth',
+    'django.contrib.contenttypes',
+    'django.contrib.sessions',
+    'django.contrib.messages',
+    'django.contrib.staticfiles',
+    'django.contrib.sites']
